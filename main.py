1f8039c9a9653150140ee871e3aa42459826e076
onlinematematika01-jpg <onlinematematika01@gmail.com>
2026-03-19 13:58:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
 from database.db import init_db
-from handlers import admin, king, lord, member, common, assassination, war, help
+from handlers import admin, king, lord, member, common, assassination, war, help, rating
 from handlers.war import process_weekly_tributes
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from middlewares.auth import AuthMiddleware
@@ -43,6 +43,7 @@ async def main():
     dp.include_router(assassination.router)
     dp.include_router(war.router)
     dp.include_router(help.router)
+    dp.include_router(rating.router)
 
     # Haftalik tribute scheduler
     scheduler = AsyncIOScheduler(timezone='Asia/Tashkent')
